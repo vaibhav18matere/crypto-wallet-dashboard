5bb94fe729a952f7dab23b2d7141200ba9c6cf65
VaibhavMatere <vpm00718897@gmail.com>
2024-08-03 02:04:13 +0530

    coinbase icon added, pricenav data separate file

--- src/components/PriceNav.jsx
+++ src/components/PriceNav.jsx
@@ -1,25 +1,21 @@
 import { BinanceIcon } from "../data/Icons";
+import { CoinBaseIcon } from "../data/Icons";
+import { priceNavData } from "../data/PriceNavData";
 
 export default function PriceNav() {
-  const data = {
-    Price: "$123,456",
-    "24 Hour Volume": "$123,456B",
-    "24 Hour Change": "-23%",
-    "Market Cap": "$23B",
-  };
 
   const getChangeColor = (value) => {
     return value.startsWith("-") ? "text-red-500" : "text-green-500";
   };
 
   return (
-    <div className="flex justify-evenly items-center container gap-4 bg-blue-200 border rounded-lg">
+    <div className="flex justify-evenly items-center gap-4 bg-blue-200 border rounded-lg w-full">
       <div className="flex gap-3">
         <BinanceIcon />
         <h1 className="text-[#F0B90B] text-2xl font-bold leading-5">Binance</h1>
       </div>
       <ul className="flex justify-evenly items-center text-lg p-4 gap-8">
-        {Object.entries(data).map(([key, value]) => (
+        {Object.entries(priceNavData).map(([key, value]) => (
           <li key={key}>
             <strong>{key}:</strong>{" "}
             <span
@@ -30,7 +26,10 @@ export default function PriceNav() {
           </li>
         ))}
       </ul>
-      <h1 className="text-green-900 text-lg font-bold">Coinbase</h1>
+      <div className="flex">
+        <CoinBaseIcon />
+        <h1 className="text-green-900 text-lg font-bold ml-2">Coinbase</h1>
+      </div>
     </div>
   );
 }
