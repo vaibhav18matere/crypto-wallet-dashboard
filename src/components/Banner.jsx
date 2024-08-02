e77eb3ab5f6dee6af278b182fc1e03f3a1ea59eb
VaibhavMatere <vpm00718897@gmail.com>
2024-08-03 02:03:29 +0530

    Banner component removed and img added

--- src/components/Banner.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { BannerIcon, Notification } from "../data/Icons";
-export default function Banner() {
-  return (
-    <div className="container bg-[#eebc35] max-w-2xl mx-auto">
-      <BannerIcon />
-    </div>
-  );
-}
