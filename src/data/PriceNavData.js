711c72a25e991c277ff37310821dc81b251dace7
VaibhavMatere <vpm00718897@gmail.com>
2024-08-03 01:58:56 +0530

    wallet and price data added

---
+++ src/data/PriceNavData.js
@@ -0,0 +1,6 @@
+export const priceNavData = {
+     Price: "$123,456",
+     "24 Hour Volume": "$123,456B",
+     "24 Hour Change": "-23%",
+     "Market Cap": "$23B",
+};
---
+++ src/data/WalletDetailsData.js
@@ -0,0 +1,24 @@
+import { Deposit, Referral, Swap, Wallet } from "./Icons";
+
+export const functionsData = [
+     {
+          title: "Deposit",
+          icon: <Deposit />,
+          bgColor: "#f0e202",
+     },
+     {
+          title: "Referral",
+          icon: <Referral />,
+          bgColor: "#c5f1ff",
+     },
+     {
+          title: "Swap",
+          icon: <Swap />,
+          bgColor: "#fed4c0",
+     },
+     {
+          title: "Wallet",
+          icon: <Wallet />,
+          bgColor: "#ecfaf0",
+     },
+];
